9052b3dd70a85a05c4f26f73426ca77a4f4f285c
brunodpoliveira <70975013+brunodpoliveira@users.noreply.github.com>
2020-10-12 20:48:08 -0300

    Update 0-credentials.py

--- naive-bayes/0-credentials.py
+++ naive-bayes/0-credentials.py
@@ -3,10 +3,10 @@ import json
 # Enter keys/secrets below as strings
 
 credentials = {}
-credentials['CONSUMER_KEY'] = 'J7UpsIsqb2Q4hhS0oqmxwxyUO'
-credentials['CONSUMER_SECRET'] = '6jnwMUXaIo1xxSqVNnhXhubwhFOoQ1tNc1VTvN1BngBWHLapZZ'
-credentials['ACCESS_TOKEN'] = '1301885931998371840-AfGQ0LSiBbdkrTFueFjPO1C0g2c9tR'
-credentials['ACCESS_SECRET'] = 'xN54XK90ci9MeXybp8tn98TWrpVZFNDUPQtGZwJGV0Nlw'
+credentials['CONSUMER_KEY'] = ''
+credentials['CONSUMER_SECRET'] = ''
+credentials['ACCESS_TOKEN'] = ''
+credentials['ACCESS_SECRET'] = ''
 
 # Save to file
 with open("twitter_credentials.json","w") as file: json.dump(credentials, file)
